21672b739c497ed85ae4e938cd4d45ec67f79c3d
riza <aldiarryskulov@normkorm.local>
2023-11-02 03:39:25 +0600

    yandexMap

--- FormValidator.js
+++ FormValidator.js
@@ -1,6 +1,13 @@
-const inputPhone = document.querySelector("#input-number")
+const inputPhone = document.querySelector("#input-number");
+const inputNumberError = document.querySelector(".input-number-error");
+const inputName = document.querySelector("#input-name");
+const inputNameError = document.querySelector(".input-name-error");
+const checkBox = document.querySelector("#terms-and-conditions")
 
 const buttonFormSubmit = document.querySelector(".button-form-submit");
+
+const modal = document.querySelector('#thank-you');
+const closeButton = document.querySelector('#close-button');
 
 const maskOptions = {
     mask: '+{7}(000)000-00-00'
@@ -8,10 +15,46 @@ const maskOptions = {
 
 const mask = IMask(inputPhone, maskOptions);
 
-console.log(mask.value)
+function openModal() {
+    modal.showModal();
+    window.addEventListener('click', closeModalOnClickOutside);
+}
+function closeModal() {
+    modal.close();
+    window.removeEventListener('click', closeModalOnClickOutside);
+}
 
+function closeModalOnClickOutside(event) {
+    if (event.target === modal) {
+        closeModal();
+    }
+}
+
+closeButton.addEventListener('click', () => {
+    closeModal();
+});
 
 buttonFormSubmit.addEventListener("click", (evt) => {
     evt.preventDefault();
-    console.log(mask.unmaskedValue)
+    if (mask.unmaskedValue.length === 11 && inputName.value.length >= 1 && checkBox.checked === true) {
+        inputNumberError.style.display = "none";
+        inputNameError.style.display = "none";
+        document.querySelector(".errors").style.display = "none";
+        inputPhone.value = "";
+        inputName.value = "";
+        checkBox.checked = false;
+        openModal();
+    } else {
+        document.querySelector(".errors").style.display = "block";
+        if (mask.unmaskedValue.length !== 11) {
+            inputNumberError.style.display = "block"
+        } else {
+            inputNumberError.style.display = "none";
+        }
+        if (inputName.value.length >= 1) {
+            inputNameError.style.display = "none";
+        } else {
+            inputNameError.style.display = "block";
+        }
+    }
 })
